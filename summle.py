cc9107b37e035ccfb03696171cbe982412e7d06f
menesesghz <gerardo.meneses.hz@gmail.com>
2022-03-27 02:40:07 -0700

    implement recursive solution for summle.net game

--- summle.py
+++ summle.py
@@ -1,33 +1,56 @@
-from itertools import permutations
+nums = [3, 3, 8, 8, 25, 100]
+target = 540
+operation = {
+    "+": lambda a,b: a+b,
+    "-": lambda a,b: a-b,
+    "/": lambda a,b: a/b,
+    "*": lambda a,b: a*b
+}
+max_iteration = 5
 
-NUMS = [1,2,6,6,11,20]
-OPERATORS = ["+" , "-","/","*"]
-TARGET = 690
+def summle_solver(target: int, nums: list, solution_chain: str = "\n" , iteration = 0):
+    if not nums or iteration > max_iteration - 1:
+        return (False, None)  
 
-nums_permed = list(permutations(NUMS))
-print(nums_permed)
+    nums_length = len(nums)
+    for i in range(nums_length):
+        a = nums[i]
+        for j in range(nums_length):
+            if i == j:
+                continue
+            for operator in operation:
+                b = nums[j]
+                if operator in ["/", "*"] and 0 in [a, b]:
+                    continue
+
+                new_num = operation[operator](a, b)
+                if new_num < 0 or new_num > target or isinstance(new_num, float):
+                    continue
+
+                new_nums = nums.copy()
+                new_nums.remove(a)
+                new_nums.remove(b)
+
+                possible_solution_chain = f"{iteration}. {a}{operator}{b} = {new_num}\n"
+                possible_solution_chain = solution_chain + possible_solution_chain
+
+                if target - new_num == 0:
+                    return (True, possible_solution_chain)
+
+                result = summle_solver(
+                    target=target,
+                    nums=new_nums + [new_num],
+                    solution_chain=possible_solution_chain,
+                    iteration=iteration+1
+                )
+                if result[0]:
+                    return result
+    return (False, None)
 
 
-def operation(opp , a, b):
-    if opp == "+":
-        return a +b
-    if opp == "-":
-        return a -b
-    if opp == "/":
-        return a/b
-    if opp == "*":
-        return a*b
+solution_chain = summle_solver(
+    target=target,
+    nums=nums,
+)
 
-
-
-
-def pop_append(arr , remove , add):
-    arr.remove(remove)
-    arr.insert(0,add)
-
-
-
-def summle(perms):
-    for i in range(len(perms)):
-
-
+print(solution_chain[1])
